022eb7a39a75a06b6a9edb4c994e1186c56b69db
Derek Koloditch <derek.koloditch@gmail.com>
2015-11-19 14:04:40 -0700

    janus install and better mappings

--- vagrant_janus_setup.sh
+++ vagrant_janus_setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# sudo yum install -y ctags
-
 # create .janus directory for plugins and such
 mkdir /home/vagrant/.janus
+cd /home/vagrant/.janus
+
+# janus
+curl -L https://bit.ly/janus-bootstrap | bash
 
 # Fuzzy Finder
-cd /home/vagrant/.janus
 wget "https://github.com/dkoloditch/dotfiles/blob/master/vim-l9.zip?raw=true"
 unzip vim-l9.zip
 git clone git@github.com:vim-scripts/FuzzyFinder.git
@@ -27,4 +28,15 @@ map <leader>h :wincmd h<CR>
 map <leader>j :wincmd j<CR>
 map <leader>k :wincmd k<CR>
 map <leader>l :wincmd l<CR>
+
+" Line movement mods
+nnoremap <S-h> <S-^>
+nnoremap <S-l> <S-$>
+
+" Redo
+nmap r :redo<Enter>
 ' > /home/vagrant/.vimrc.after
+
+# Vim-Tags (like Ctags)
+# sudo yum install -y ctags
+# git clone https://github.com/szw/vim-tags.git
